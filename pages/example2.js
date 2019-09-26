839c4391ed49c0acca6c6f44e986bcd739c1650b
Alexandr Martirosyan <alexandrmenua@gmail.com>
2019-09-26 19:00:17 +0400

    changes

--- pages/example2.js
+++ pages/example2.js
@@ -1,24 +1,31 @@
 import Web3, { providers } from 'web3';
+import DiceGame from './build/DiceGame.json';
 
 let provider = new providers.HttpProvider('https://rinkeby.infura.io/v3/a8bc12d19ee2426eba8ab41aedce8f10');
 
 const web3 = new Web3(provider);
 
 let privatKey = '0x0CDC9A772C15F9409B328B0119A5486D15F2312E65F28C387C4D9BEAF17AE674';
-let managerAccount = web3.eth.accounts.privateKeyToAccount(privatKey);
+let contract = new web3.eth.Contract(
+    DiceGame.abi,
+    '0xB1a6A24Ea7427f3f09CBB43Fb3b5d3d9930d304D'
+);
 
-const newGame = async () => {
+const newGame = async (dice, minimumBet) => {
+    if (dice < 1 || dice > 12 || isNaN(minimumBet)) return false;
+    const tr = contract.methods.newGame(dice, minimumBet);
 
+    
 }
 
 const signAndSendTransaction = async (senderPrivateKey, to, encodeABI, gas) => {
     let pk = senderPrivateKey;
     if ('0x' !== pk.slice(0, 2)) {
-            pk = '0x' + pk;
+        pk = '0x' + pk;
     }
 
     let acc = web3.eth.accounts.privateKeyToAccount(pk);
-    
+
     const transaction = {
         from: acc.address,
         to: to,
@@ -27,9 +34,8 @@ const signAndSendTransaction = async (senderPrivateKey, to, encodeABI, gas) => {
     }
 
     let signed = await web3.eth.accounts.signTransaction(transaction, pk);
-    let trn = web3.eth.sendSignedTransaction(signed.rawTransaction);
-
-    
+    let trn = await web3.eth.sendSignedTransaction(signed.rawTransaction);
+    return true;
 }
 
 
